ee5b0dd1b4aa4ef5d8227fe60084575aef2ba030
Mohanasriharan J <87821989+Mohanasriharan@users.noreply.github.com>
2023-11-20 19:09:50 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'src/assets/bb.png',
+    title: "Phishing website",
+    desc: " Although many methods have been proposed to detect phishing websites, Phishers have evolved their methods to escape from these detection methods. One of the most successful methods for detecting these malicious activities is Machine Learning.",
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7118490605571215360/",
+    git: "https://github.com/Mohanasriharan/Final-year-project",
+  },
+  {
+    id: 2,
+    img: 'src/assets/nn.png',
+    title: "Medico recommender",
+    desc:"I've developed a machine learning model that can classify the condition of a patient based on the review given by the patient. I've used the dataset from the UCI machine learning repository. I've used the Naive Bayes algorithm to classify the condition of the patient. I've used the Flask framework to deploy the model.",
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7118487854153965568/",
+    git: "https://github.com/Mohanasriharan/Drug_review_condition_classification",
+  },
+  {
+    id: 3,
+    img: 'src/assets/zz.png',
+    title: "Road accident analysis",
+    desc: "I've employed the power of Tableau to analyze and visualize accident data. One of the key features of my project is the utilization of different sliders, allowing users to explore and understand various aspects of road accidents.",
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7068160902746816512/",
+    git: "https://github.com/Mohanasriharan/Road_accident_tableau",
+  }
+  
+];
